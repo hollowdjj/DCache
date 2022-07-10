9be6255cff27cf432a6bf6314fbb21425e88570d
hollowdjj <1187636437@qq.com>
2022-07-10 10:48:19 +0800

    bug fix

--- http.go
+++ http.go
@@ -15,7 +15,7 @@ const (
 type HttpPool struct {
 	mu sync.Mutex
 
-	//本机地址 eg:http://xx.xx.xxx.xx:8000
+	//本机地址 eg:xx.xx.xxx.xx:8000
 	selfAddr string
 
 	//一致性哈希
@@ -39,7 +39,7 @@ func (h *HttpPool) SetPeers(addrs ...string) {
 	h.hash = consistent.New(defaultReplicas, nil)
 	h.peers = make(map[string]Peer)
 	for _, addr := range addrs {
-		h.peers[addr] = &httpPeer{remoteBaseUrl: addr + defaultRoute}
+		h.peers[addr] = &httpPeer{remoteBaseUrl: "http://" + addr + defaultRoute}
 	}
 	h.hash.AddNodes(addrs...)
 }
